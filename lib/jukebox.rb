f23f076f5aaacc081089676af0f305b08b3020b3
Daniel Phillips <danphi@gmail.com>
2020-01-30 06:10:52 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,63 @@
-# Add your code here
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+  
+  song = nil
+  
+  songs.each_with_index do |s, i|
+    if input == (i+1).to_s
+      song = s
+    elsif input == s
+      song = s
+    end
+  end
+  
+  if song == nil
+    puts "Invalid input, please try again"
+  else
+    puts "Playing #{song}"
+  end
+end
+
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+  end
+end
+
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+
+def run(songs)
+  puts "Please enter a command:"
+  
+  isRunning = true
+  
+  while isRunning do
+    input = gets.strip
+    
+    if input == "help"
+      help
+    elsif input == "play"
+      play(songs)
+    elsif input == "list"
+      list(songs)
+    elsif input == "exit"
+      exit_jukebox
+      isRunning = false
+    end
+  end
+end
